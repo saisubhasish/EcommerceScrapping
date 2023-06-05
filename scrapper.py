f968b066e81031d2f2c9360cd670c3ae770326cf
saisubhasish <saisubhasishrout777@gmail.com>
2023-06-05 14:21:42 +0530

    first commit

---
+++ scrapper.py
@@ -0,0 +1,105 @@
+import os
+import time
+import requests
+from selenium import webdriver
+
+
+def fetch_image_urls(comapny: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 1):
+    '''
+    This is the function to scrap images from website
+    '''
+    def scroll_to_end(wd):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_between_interactions)
+
+        # build the google query
+
+    search_url = "https://www.{company}.com/"
+
+    # load the page
+    wd.get(search_url)
+
+    image_urls = set()
+    image_count = 0
+    results_start = 0
+    while image_count < max_links_to_fetch:
+        scroll_to_end(wd)
+
+        # get all image thumbnail results
+        thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
+        number_results = len(thumbnail_results)
+
+        print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
+
+        for img in thumbnail_results[results_start:number_results]:
+            # try to click every thumbnail such that we can get the real image behind it
+            try:
+                img.click()
+                #print(type(img))   
+                time.sleep(sleep_between_interactions)
+            except Exception:
+                continue
+
+            # extract image urls
+            actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
+            for actual_image in actual_images:
+                if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
+                    image_urls.add(actual_image.get_attribute('src'))
+
+            image_count = len(image_urls)
+
+            if len(image_urls) >= max_links_to_fetch:
+                print(f"Found: {len(image_urls)} image links, done!")
+                break
+        else:
+            print("Found:", len(image_urls), "image links, looking for more ...")
+            time.sleep(30)
+            return
+            load_more_button = wd.find_element_by_css_selector(".mye4qd")
+            if load_more_button:
+                wd.execute_script("document.querySelector('.mye4qd').click();")
+
+        # move the result startpoint further down
+        results_start = len(thumbnail_results)
+
+    return image_urls
+
+
+def persist_image(folder_path:str,url:str, counter):
+    try:
+        image_content = requests.get(url).content
+
+    except Exception as e:
+        print(f"ERROR - Could not download {url} - {e}")
+
+    try:
+        f = open(os.path.join(folder_path, 'jpg' + "_" + str(counter) + ".jpg"), 'wb')
+        f.write(image_content)
+        f.close()
+        print(f"SUCCESS - saved {url} - as {folder_path}")
+    except Exception as e:
+        print(f"ERROR - Could not save {url} - {e}")
+
+
+def search_and_download(company: str, driver_path: str, target_path='./images', number_images=10):
+    target_folder = os.path.join(target_path, '_'.join(company.lower().split(' ')))
+
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+
+    with webdriver.Chrome(executable_path=driver_path) as wd:
+        res = fetch_image_urls(company, number_images, wd=wd, sleep_between_interactions=0.5)
+
+    counter = 0
+    for elem in res:
+        persist_image(target_folder, elem, counter)
+        counter += 1
+
+
+
+
+
+DRIVER_PATH = r'D:\FSDS-iNeuron\10.Projects-DS\Image_Scrapper\chromedriver.exe'
+company = 'flipkart'
+
+search_and_download(company=company, driver_path=DRIVER_PATH, number_images=50)
